083e52855a7421595a0c1f1ce36712942ca3e561
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-10-07 17:37:33 -0400

    added env for mongodb

--- server/dal.js
+++ server/dal.js
@@ -11,7 +11,7 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = "mongodb+srv://tinacollier:B1RsnSpltJaEP9mo@badbankinstance.wpy6j.mongodb.net/?retryWrites=true&w=majority";
 let db;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient( process.env.MONGODB_URI || uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 try {
     client.connect(err => {
         db = client.db("tieredbadbank");
