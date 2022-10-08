c310a9371b377c866ddf44ff5a5e33500496caf9
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-10-08 17:32:40 -0400

    working on dal.js functions

--- server/index.js
+++ server/index.js
@@ -31,8 +31,8 @@ app.get( "/api", ( req, res ) => {
   } );
 
 // create user account
-app.post( '/user/create', jsonParser, ( req, res ) => {
-  dal.create( req.body.name, req.body.email, req.body.password ).then( ( user ) => {
+app.post( '/user/create', jsonParser, async ( req, res ) => {
+  await dal.create( req.body.name, req.body.email, req.body.password ).then( ( user ) => {
       res.send( user );
     } );
 } );
