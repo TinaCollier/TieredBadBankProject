22cfded6b75b739ab0f5d4d587be83a1f2bdb815
Tina Collier <tinacollier@Tinas-MacBook-Pro.local>
2022-10-09 15:29:53 -0400

    trying to fix create account issues

--- public/src/components/Form.js
+++ public/src/components/Form.js
@@ -40,7 +40,7 @@ const Form = () => {
     await fetch( uri, requestOptions)
       .then( response => response.json() )
       .then( data => {
-        console.log( 'form data', data );
+        console.log( 'form data', JSON.stringify(data) );
         // setId( data.insertId);
       } ); 
       // setName( values.name );
